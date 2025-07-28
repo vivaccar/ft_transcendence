74a35ba565d48497b60aa11ede0b3e5f42d67ab2
Amauri Lisboa <amauri.olisboa@gmail.com>
2025-07-28 20:04:39 +0100

    API GATEWAY Base to start working - With Typescript - See docs on notion if necessary

---
+++ api-gateway/dist/server.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const fastify = (0, fastify_1.default)({
+    logger: true,
+});
+fastify.get('/', async (request, reply) => {
+    return { hello: 'typescript world' };
+});
+// 4. Função para iniciar o servidor
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 });
+    }
+    catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
