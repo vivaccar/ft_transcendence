42edd3612632c1df5c8d26baf884d10bfa5203a7
vivaccar <viniciusvaccarih@hotmail.com>
2025-08-19 16:29:02 +0100

    cookies finally working with google and regular login

--- backend/srcs/app.ts
+++ backend/srcs/app.ts
@@ -27,12 +27,11 @@ app.register(cors, {
   origin: ['https://localhost', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
 });
 
-app.register(fastifyCookie, {
+/* app.register(fastifyCookie, {
   secret: process.env.COOKIE_SECRET!,
-})
+}) */
 app.register(swaggerPlugin)
 app.register(dbPlugin);
 app.register(jwtPlugin);
